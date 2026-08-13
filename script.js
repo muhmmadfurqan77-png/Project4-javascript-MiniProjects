c06667db49704da22776ef2aea508f165fb7c603
muhmmadfurqan77-png <furqanislam77@gmail.com>
2026-08-12 23:29:37 -0700

    Add Project 7 - Grammar Checker

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
-const input = 'turpentine and turtles';
+let story = 'Last weekend, I took literally the most beautiful trip to the beach. I went to the beach with my friends and we literally had a great time. The waves were literally crashing and the sand was literally so hot. We ate literally sandwiches and drank literally lemonade. It was literally the best day ever.';
 
-const vowels = ['a', 'e', 'i', 'o', 'u'];
+let storyWords = story.split(' ');
+console.log(storyWords);
 
-let resultArray = [];
+console.log(storyWords.join(' '));
 
-for (let i = 0; i < input.length; i++) {
-  for (let j = 0; j < vowels.length; j++) {
-    if (input[i] === vowels[j]) {
-      resultArray.push(input[i]);
-    }
+let count = 0;
+storyWords.forEach(word => {
+  count++;
+});
+console.log(count);
+
+storyWords = storyWords.filter(word => word!== 'literally');
+console.log(storyWords.join(' '));
+
+storyWords = storyWords.map(word => {
+  if (word === 'beautiful') {
+    return 'stunning';
+  } else {
+    return word;
   }
-  
-  if (input[i] === 'e') {
-    resultArray.push(input[i]);
-  }
-  
-  if (input[i] === 'u') {
-    resultArray.push(input[i]);
-  }
-};
+});
+console.log(storyWords.join(' '));
 
-const resultString = resultArray.join('').toUpperCase();
+const badWordIndex = storyWords.findIndex(word => word === 'sandwiches');
+storyWords[badWordIndex] = 'snacks';
+console.log(storyWords.join(' '));
 
-console.log(resultString);
+let longWord = storyWords.find(word => word.length > 10);
+console.log(longWord);
