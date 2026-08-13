2aecea2e3eaa9656660e9bf0d1ff0e0e86e775db
muhmmadfurqan77-png <furqanislam77@gmail.com>
2026-08-12 23:27:08 -0700

    Add Project 5 - Training Days

--- script.js
+++ script.js
@@ -1,47 +1,44 @@
-const getSleepHours = day => {
-  if (day === 'monday') {
-    return 8;
-  } else if (day === 'tuesday') {
-    return 7;
-  } else if (day === 'wednesday') {
-    return 6;
-  } else if (day === 'thursday') {
-    return 8;
-  } else if (day === 'friday') {
-    return 5;
-  } else if (day === 'saturday') {
-    return 9;
-  } else if (day === 'sunday') {
-    return 8;
+const getRandEvent = () => {
+  const random = Math.floor(Math.random() * 3);
+  if (random === 0) {
+    return 'Marathon';
+  } else if (random === 1) {
+    return 'Triathlon';
+  } else if (random === 2) {
+    return 'Pentathlon';
   }
 };
 
-const getActualSleepHours = () => {
-  return getSleepHours('monday') + 
-  getSleepHours('tuesday') + 
-  getSleepHours('wednesday') + 
-  getSleepHours('thursday') + 
-  getSleepHours('friday') + 
-  getSleepHours('saturday') + 
-  getSleepHours('sunday');
-};
-
-const getIdealSleepHours = () => {
-  const idealHours = 8;
-  return idealHours * 7;
-};
-
-const calculateSleepDebt = () => {
-  const actualSleepHours = getActualSleepHours();
-  const idealSleepHours = getIdealSleepHours();
-  
-  if (actualSleepHours === idealSleepHours) {
-    console.log('You got the perfect amount of sleep');
-  } else if (actualSleepHours > idealSleepHours) {
-    console.log('You got ' + (actualSleepHours - idealSleepHours) + ' hours more sleep than needed');
-  } else {
-    console.log('You should get ' + (idealSleepHours - actualSleepHours) + ' hours of rest');
+const getTrainingDays = event => {
+  let days;
+  if (event === 'Marathon') {
+    days = 50;
+  } else if (event === 'Triathlon') {
+    days = 100;
+  } else if (event === 'Pentathlon') {
+    days = 200;
   }
+  return days;
 };
 
-calculateSleepDebt();
+const logEvent = (name, event) => {
+  console.log(`${name}'s event is: ${event}`);
+};
+
+const logTime = (name, days) => {
+  console.log(`${name}'s time to train is: ${days} days`);
+};
+
+const name = 'Nala';
+const event = getRandEvent();
+const days = getTrainingDays(event);
+
+logEvent(name, event);
+logTime(name, days);
+
+const name2 = 'Warren';
+const event2 = getRandEvent();
+const days2 = getTrainingDays(event2);
+
+logEvent(name2, event2);
+logTime(name2, days2);
